3b5a2db5d4cffe2327c341b77d5baa298f2165e1
ilaibaaxh <126773435+ilaibaaxh@users.noreply.github.com>
2023-06-27 23:23:41 +0530

    added

---
+++ req.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+
+
+backBtn.addEventListener("click", () => form.classList.backto('index.html'));
